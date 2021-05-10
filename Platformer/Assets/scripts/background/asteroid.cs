448c1e097d74a8009826c1a9b6fa40a09e275f8c
felicijus <77450740+felicijus@users.noreply.github.com>
2021-05-10 23:13:43 +0200

    complete asteroid script

--- Platformer/Assets/scripts/background/asteroid.cs
+++ Platformer/Assets/scripts/background/asteroid.cs
@@ -1,33 +1,48 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;using UnityEditor.UIElements;
+using System.Collections.Generic;using TMPro;using UnityEditor.UIElements;
 using UnityEngine;
-
-
-
 
 public class asteroid : MonoBehaviour
 {
     public float speed;
     public float border;
 
-    private Vector3 screenBounds;
+    public Vector3 screenReference;
+    public Vector3 screenUpdate;
+    public Vector3 screen;
+
+    public Vector3 edgeleft;
+    public Vector3 edgeright;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenReference = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,Camera.main.transform.position.z));
+        screenUpdate = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,Camera.main.transform.position.z));
+        
+        screen.x = (screenUpdate.x - screenReference.x);
+
+        edgeleft.x = screen.x + screenReference.x;
+        edgeright.x = screen.x - screenReference.x;
+        
         
         transform.position = transform.position + new Vector3(speed,0 , 0);
+        
+        //Output the current screen window width in the console
+        //Debug.Log("Screen Width : " + screenReference.x);
+        //Debug.Log("Screen Width : " + screenUpdate.x);
+        //Debug.Log("Screen Width : " + screen.x);
 
-        if (transform.position.x < (screenBounds.x - border))
+        if (transform.position.x < (edgeleft.x - border))
         {
-            transform.position = new Vector3(-screenBounds.x + border, transform.position.y, transform.position.z);
+            transform.position = new Vector3((edgeright.x + border), transform.position.y, transform.position.z);
         }
         
 
---
+++ Platformer/Assets/scripts/player/camera.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float speed = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+        
+        Vector3 position = this.transform.position;
+        position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interpolation);
+        
+        this.transform.position = position;
+    }
+}
