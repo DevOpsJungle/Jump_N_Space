cdeea8ca8039a0ea091aafac55d1c0b1e8a701c1
goldentoastmassageteam <goldentoastmassageteam@gmail.com>
2021-06-14 22:36:23 +0200

    1st Main menu

--- Platformer/Assets/Scripts/Menu/Rocket.cs
+++ Platformer/Assets/Scripts/Menu/Rocket.cs
@@ -6,29 +6,28 @@ using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
-    Vector3 vector,position;
+    Vector3 vector;
     float movingspeed = 100.0f;
     Rigidbody r_rigidbody;
     private int currentstate;
-    public Sprite[] rocket=new Sprite[4];
+    public Sprite[] rocket=new Sprite[4];  /*array with all used images*/
     
     // Start is called before the first frame update
     void Start()
     {
-        vector = new Vector3(-1.0f, 1.0f, 0.0f);
-        position = new Vector3(-300.0f, 300.0f, 0.0f);
-        r_rigidbody = GetComponent<Rigidbody>();
+        vector = new Vector3(-1.0f, 1.0f, 0.0f); /*set direction*/
+        r_rigidbody = GetComponent<Rigidbody>(); /*assign rigitbody*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        r_rigidbody.velocity = vector * movingspeed;
-        if (currentstate != 0 && currentstate%60==0)
+        r_rigidbody.velocity = vector * movingspeed; /*set velocity*/
+        if (currentstate != 0 && currentstate%60==0) /*animation change every 60 frames*/
         {
             gameObject.GetComponent<Image>().sprite = rocket[currentstate/60];
             currentstate++;
-            if ((currentstate-1) / 60 == 3)
+            if ((currentstate-1) / 60 == 3) /*if the last image is shown, begin with the first*/
             {
                 currentstate = 0;
             }
@@ -37,11 +36,10 @@ public class Rocket : MonoBehaviour
         {
             currentstate++;
         }
-        Debug.Log(Vector3.Distance(r_rigidbody.position,position));
+        
         if (r_rigidbody.position.y>600)
         {
-            //r_rigidbody.position = Vector3.zero;
-            gameObject.GetComponent<Image>().rectTransform.anchoredPosition3D = new Vector3(300,-290,0);
+            gameObject.GetComponent<Image>().rectTransform.anchoredPosition3D = new Vector3(600,-450,0); /*reset image*/
         }
     }
 }
