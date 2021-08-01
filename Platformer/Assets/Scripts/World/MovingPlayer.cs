254f1e4bcacd5a490a264a0c62003fc4fe8d92f3
Vincent <vincent.31@web.de>
2021-08-01 20:19:39 +0200

    Chunks

---
+++ Platformer/Assets/Scripts/World/MovingPlayer.cs
@@ -0,0 +1,44 @@
+/*
+ * Script: Moving Player
+ * Author: Vincent Becker
+ * Last Change: 01.08.21
+ * The script sets the player as a child of the moving platform so that the platform can move horizontally
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector3 velocity;
+    private bool moving;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+            
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(null);
+        }
+            
+    }
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            transform.position += (velocity * Time.deltaTime);
+        }
+    }
+}
